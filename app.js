ef162d689c4df9e38dbe30f27ce719d98e359be4
Volodymyr025 <118533426+Volodymyr025@users.noreply.github.com>
2023-06-19 17:02:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 const load = document.getElementById('load-page')
 const btn = document.getElementById('btn')
-// const btn = document.getElementById('but').addEventListener('click', ()=>{
-//   fetch('https://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.log(error));
-//   })
+const userTable = document.getElementById('table')
 
-async function loading () {
-  load.style.display = 'flex'
-  const loadProm = new Promise ((resolve,reject) => {
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+const timeOut = ms => {
+  return new Promise ((resolve,reject) => {
     setTimeout(()=>{
       load.style.display = 'none'
       resolve()
-    },6000)
-  }).then(() =>{ 
-    console.log('dsds')
-})
+    },ms)
+  })
+}
+
+function visible (item) {
+  item.style.display = 'flex'
+}
+
+function addListUser (userData){
+  const post = document.querySelector('.post')
+  for(const dataList of userData){
+    const elTr = document.createElement('tr')
+    const elTd = document.createElement('td')
+    let itemEl = ''
+    itemEl += `<td>${dataList.name}</td>
+    <td>${dataList.username}</td>
+    <td>${dataList.email}</td>
+    <td>${dataList.phone}</td>`
+    elTr.innerHTML = itemEl
+    post.append(elTr)
+  }
+}
+
+
+async function loading () {
+  visible(load)
+  try{
+    await timeOut(5000)
+    const response = await fetch(url)
+    const data = await response.json()
+    await visible(userTable)
+    await addListUser(data)
+  }
+  catch (e) {
+    console.error(e)
+  }
 }
 
 btn.addEventListener('click',loading)
